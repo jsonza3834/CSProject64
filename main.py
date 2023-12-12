ef07e5d6b75c1a4ba40baadbc5ac65b75bc1e730
Trent Anderson <trentma21@gmail.com>
2023-12-12 18:11:31 -0500

    Commented code

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+#main.py
 from controller import Controller
 from view import View
 from model import Model
 
 
-if __name__ == "__main__":
-       
-    #output_file_path = "cleaned.wav"  
-
-    
+if __name__ == "__main__":      
     # Model
     model = Model()
 
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+#modle.py
 from pydub import AudioSegment
 import os
 import tempfile
@@ -13,11 +14,8 @@ import matplotlib.pyplot as plt
 
 
 class Model:
-    def __init__(self):
-        #self.file_path = 
-        #self.wav_path = wav_path
-        #self.audio_data = self.read_audio()
-        
+    # Initialize variable for calculations
+    def __init__(self):       
         self.samplerate = 0
         self.data = None
         self.channels = 0
@@ -26,8 +24,7 @@ class Model:
         self.t = None
         self.im = None
 
-
-    
+    # Read the audio file
     def read_audio(self, file_path):
         try:
             audio = AudioSegment.from_file(file_path)
@@ -36,11 +33,13 @@ class Model:
             print(f"Error loading audio file: {e}")
             return None
 
+    # Convert the audio file to .wav
     def convert_to_wav(self, audio_data, output_path):
         # Convert to wav format
         if format != "wav":
             audio_data.export(output_path, format="wav")
 
+    # Read the newly created w.av file
     def read_wav(self, wav_path):
         try:
             wav = AudioSegment.from_file(wav_path)
@@ -48,17 +47,18 @@ class Model:
         except Exception as e:
             print(f"Error loading wav file: {e}")
             return None
-        
+
+    # Convert the file to one channel
     def mono_channel(self, wav_file, output_path):
         wav_file = wav_file.set_channels(1)
         wav_file.export(output_path, format="wav")       
             
-
+    # Remove metadat from the file using os
     def remove_metadata(self, wav_file, output_path):
         # Use ffmpeg to remove metadata
         os.system(f'ffmpeg -i "{wav_file}" -map_metadata -1 -acodec copy "{output_path}"')
 
-
+    # Analyze the file and grab data
     def analyze_file(self, file_path):
         wav_fname = file_path
         with wave.open(wav_fname, 'rb') as wav_read:
@@ -66,18 +66,21 @@ class Model:
         self.samplerate, self.data = wavfile.read(wav_fname)
         self.spectrum, self.freqs, self.t, self.im = plt.specgram(self.data, Fs=self.samplerate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
 
-
+    # Calculate the resonance
     def calculate_res(self, file_path):
         frequencies, power = welch(self.data, self.samplerate, nperseg=4096)
         dominant = frequencies[np.argmax(power)]
         return dominant
-
+    
+    # Calulate RT60 values
     def RT60(self, low_freq, high_freq):
         # Find indices of frequencies within the specified range
         indices = np.where((self.freqs >= low_freq) & (self.freqs <= high_freq))[0]
+        
         # Find the frequency with maximum intensity within the specified range
         self.target_frequency_index = indices[np.argmax(np.max(self.spectrum[indices, :], axis=1))]
-            
+
+        
         data_in_db = self.frequency_check(self.freqs[self.target_frequency_index])
 
         value_of_max_less_5 = data_in_db[self.target_frequency_index] - 5
@@ -87,34 +90,34 @@ class Model:
         value_of_max_less_25 = data_in_db[self.target_frequency_index] - 25
         value_of_max_less_25 = self.find_nearest_value(data_in_db, value_of_max_less_25)
         index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
-        
+
+        # RT20 value
         rt20 = (self.t[index_of_max_less_5] - self.t[index_of_max_less_25])[0]
 
-        # calculating the RT60 value
+        # Calculating the RT60 value
         self.rt60 = 3 * rt20
 
         return abs(self.rt60)
-        
+
+    # Calculate the RT60 dif
     def RT60_dif(self, low, mid, high):
         RT60avg = (low + mid + high) / 3
         dif = RT60avg - .5
-        return round(dif,2)
-            
-    def find_target_frequency(self, freqs):
-            for x in freqs:
-                if x > 1000:
-                    break
-                return x
+        return abs(round(dif,2))
 
+
+    # Checking for frequency
     def frequency_check(self, target_frequency):
-        # identify a frequency to check
+        
+        # Identify a frequency to check
         index_of_frequency = np.where(self.freqs == target_frequency)[0][0]
-        # find sound data for a particular frequency
+        # Find sound data for a particular frequency
         data_for_frequency = self.spectrum[index_of_frequency]
-        # change a digital signal for a value in decibels
+        # Change a digital signal for a value in decibels
         data_in_db_fun = 10 * np.log10(data_for_frequency)
         return data_in_db_fun
 
+    # Find the nearest values for plotting
     def find_nearest_value(self, array, value):
         array = np.asarray(array)
         idx = (np.abs(array - value)).argmin()
